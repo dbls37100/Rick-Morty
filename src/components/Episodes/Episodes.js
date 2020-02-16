cb3c040e525f1cbe0acfa59e1ff49bb14b5fc85b
dylan belouis <dylan.belouis@gmail.com>
2020-02-16 01:22:12 +0100

    change style with material ui

--- src/components/Episodes/Episodes.js
+++ src/components/Episodes/Episodes.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Episodes.css';
 import { Button } from '@material-ui/core';
-
+import { Link } from 'react-router-dom';
 
 const Episodes = (props) => {
     // donnée générale
@@ -10,21 +10,11 @@ const Episodes = (props) => {
     // Récuperation props paramètre url
     const { match }  = props;
     let { id }       = match.params;
-    const [ idUrl , setIdUrl] = useState(id)
     // locale storage url de recherche api
     const urlApi2 =`https://rickandmortyapi.com/api/character/${id}`;
 
 
 
-    const previousPage = (e) => {
-        if(id > 1){
-            setIdUrl(id - 1);
-        }
-    }
-
-    const nextPage = (e) => {
-        setIdUrl(id + 1);
-    }
 
     // ComponentDidMount/DidUpdate
 
@@ -34,12 +24,12 @@ const Episodes = (props) => {
         .then(res => {
             setApiData(res.data);
           })
-    },[ urlApi2, id, idUrl]);
+    },[ urlApi2, id]);
 
     return (
         <div className="style-card">
             <div className="style-card_Solo">
-                <h2>{apiData.name}</h2>
+                <h1>{apiData.name}</h1>
                 <h2>{apiData.status}</h2>
                 <img className="img-Size" src={apiData.image} alt="rick et compagnie"/>
                 <h2>{apiData.species}</h2>
@@ -47,12 +37,11 @@ const Episodes = (props) => {
                 <h2>{apiData.type}</h2>
             </div>
             <div className="Footer-button_display">
-                <div className="bouton-left">
-                    <Button variant="contained" color="primary" onClick={previousPage}>{id - 1}</Button> 
-                </div>
-                <div className="bouton-right">
-                <Button variant="contained" color="primary" onClick={nextPage}>{id + 1}</Button>
-                </div>
+            <Link to="/">
+                <Button renderAs="button" variant="contained" color="primary">
+                    <span>Page précédente</span>
+                </Button>
+            </Link>
         </div>
         </div>
     )
